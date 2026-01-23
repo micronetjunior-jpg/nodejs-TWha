bd8b4162acaa3ac81236da61313b60c977fa0738
Domentia <micronetjunior@icloud.com>
2026-01-23 13:02:25 -0500

    Actualizar WebRTS.js

--- WebRTS.js
+++ WebRTS.js
@@ -1,1 +1,27 @@
-s
+const mediasoup = require('mediasoup');
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const config = require('./config');
+
+(async () => {
+  const app = express();
+  const server = http.createServer(app);
+  const io = new Server(server);
+
+  const worker = await mediasoup.createWorker(config.mediasoup.worker);
+  const router = await worker.createRouter(config.mediasoup.router);
+
+  io.on('connection', socket => {
+    console.log('Cliente conectado');
+
+    socket.on('getRtpCapabilities', (_, cb) => {
+      cb(router.rtpCapabilities);
+    });
+  });
+
+  server.listen(3000 () =>
+    console.log('MediaSoup corriendo en puerto 3000')
+  );
+  
+})();
